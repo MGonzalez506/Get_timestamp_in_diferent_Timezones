b172a46d7676c5deebf07f8c9198bee870203568
mgonzalez506 <MGonzalez506@users.noreply.github.com>
2023-09-23 10:27:18 -0600

    Configurar folder actual y funcionalidad de Log

--- testtz.py
+++ testtz.py
@@ -8,3 +8,8 @@ import unittest
 # Import the functions to test
 from timestamp_timezones import *
 
+# Get the current directory
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+
+logging.basicConfig(filename='Logs/unit_test.log', filemode='a', format='%(name)s - %(levelname)s - %(message)s')
+
